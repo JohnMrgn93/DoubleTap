803d33d50a0d0bda30e3443b791b16d433dd34dd
John Morgan II <102772504+JohnMrgn93@users.noreply.github.com>
2022-05-16 08:52:28 -0500

    Double Tap project

---
+++ index.js
@@ -0,0 +1,35 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as Store from "./Store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
+function render(state = Store.Home) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(state)}
+    ${Nav(Store.Links)}
+    ${Main(state)}
+    ${Footer()}
+  `;
+  router.updatePageLinks();
+  AfterRender();
+
+  function AfterRender() {
+    document
+      .querySelector(".fa-bars")
+      .addEventListener("click", () =>
+        document.querySelector("nav > ul").classList.toggle("hidden--mobile")
+      );
+  }
+
+  router
+    .on({
+      "/": () => render(),
+      ":view": params => {
+        let view = capitalize(params.data.view);
+        render(Store[view]);
+      }
+    })
+    .resolve();
+}
