4aae48c9b0b8dfc7c870758133a56e4f66ba2d7f
John Morgan II <102772504+JohnMrgn93@users.noreply.github.com>
2022-05-26 18:54:15 -0500

    upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Header, Nav, Main, Footer } from "./components";
+import { Header, Nav, Main, Footer } from "./components/views";
 import * as Store from "./Store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
